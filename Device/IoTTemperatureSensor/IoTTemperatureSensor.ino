a519769f7de386d826763854a8690b989624d864
Bao Nguyen <bao.nguyen@smoovapp.com>
2017-12-18 15:03:31 +0700

    Add code socket io

--- Device/IoTTemperatureSensor/IoTTemperatureSensor.ino
+++ Device/IoTTemperatureSensor/IoTTemperatureSensor.ino
@@ -1,6 +1,7 @@
 #include <DHT.h>
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <SocketIoClient.h>
 
 //#define           DEBUG
 #ifdef DEBUG
@@ -16,49 +17,69 @@
  */
 #define DHTPIN      5
 #define DHTTYPE     DHT11
-#define SSID        GetZ
-#define PASSWORD    G3tzP@ss
+#define SSID        "GetZ"
+#define PASSWORD    "G3tzP@ss"
+const char * host = "192.168.1.41";
+int port = 9898;
 
 /*
  * Global variable
  */
 DHT dht(DHTPIN, DHTTYPE);
 WiFiClient wifiClient;
+SocketIoClient webSocket;
+
+void event(const char *payload, size_t length) {
+  DEBUG_PRINTLN("Got message: %s", payload);
+}
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  DEBUG_PRINTLN("DHT Text!!!");
-  dht.begin();
+  Serial.begin(115200);
+  delay(10);
+  DEBUG_PRINT("Connecting to wifi ");
+  WiFi.begin(SSID, PASSWORD);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    DEBUG_PRINT(".")
+  }
+  DEBUG_PRINTLN(F("Conntected wifi"));
+  DEBUG_PRINTLN(F("Wifi address: %s", Wifi.localIP()));
+ 
+  webSocket.on("connection", event);
+   webSocket.begin(host, port);
+//  DEBUG_PRINTLN("DHT Text!!!");
+//  dht.begin();
 }
 
 void loop() {
+  webSocket.loop();
   // put your main code here, to run repeatedly:
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
-  float f = dht.readTemperature(true);
-  
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    DEBUG_PRINT("Incorrect data");
-    return;
-  }
-
-  float hif = dht.computeHeatIndex(f, h);
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  DEBUG_PRINT("Humidity: ");
-  DEBUG_PRINT(h);
-  DEBUG_PRINT(" %\t");
-  DEBUG_PRINT("Temperature: ");
-  DEBUG_PRINT(t);
-  DEBUG_PRINT(" *C ");
-  DEBUG_PRINT(f);
-  DEBUG_PRINT(" *F\t");
-  DEBUG_PRINT("Heat index: ");
-  DEBUG_PRINT(hic);
-  DEBUG_PRINT(" *C ");
-  DEBUG_PRINT(hif);
-  DEBUG_PRINTLN(" *F");
+//  float h = dht.readHumidity();
+//  float t = dht.readTemperature();
+//  float f = dht.readTemperature(true);
+//  
+//  if (isnan(h) || isnan(t) || isnan(f)) {
+//    DEBUG_PRINT("Incorrect data");
+//    return;
+//  }
+//
+//  float hif = dht.computeHeatIndex(f, h);
+//  float hic = dht.computeHeatIndex(t, h, false);
+//
+//  DEBUG_PRINT("Humidity: ");
+//  DEBUG_PRINT(h);
+//  DEBUG_PRINT(" %\t");
+//  DEBUG_PRINT("Temperature: ");
+//  DEBUG_PRINT(t);
+//  DEBUG_PRINT(" *C ");
+//  DEBUG_PRINT(f);
+//  DEBUG_PRINT(" *F\t");
+//  DEBUG_PRINT("Heat index: ");
+//  DEBUG_PRINT(hic);
+//  DEBUG_PRINT(" *C ");
+//  DEBUG_PRINT(hif);
+//  DEBUG_PRINTLN(" *F");
   delay(2000);
   
  }
---
+++ Server/index.js
@@ -0,0 +1,35 @@
+const PORT = 9898;
+var path = require('path');
+var bodyParser = require('body-parser');
+var express = require('express');
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+app.use(bodyParser.json());
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, done) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With, access_token");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
+    res.header("Access-Control-Allow-Credentials", "true");
+    return done();
+});
+
+app.get('/', function(req, res) {
+    res.send('Welcome to Vietlott Application!');
+});
+
+server.listen(PORT, function() {
+    console.log('[INFO]App listen at port: ' + PORT);
+});
+
+io.on('connection', function(client) {
+    console.log('Client is connected...');
+    console.log(client);
+    client.on('join', function(data) {
+        console.log(data);
+    });
+});
